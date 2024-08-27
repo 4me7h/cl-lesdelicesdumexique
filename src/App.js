e71d74e7069b897e0cce935d46786c797fda9528
4me7h <4573r10n@gmail.com>
2024-08-27 09:07:26 -0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import About from './components/About';
+// import About from './components/About';
 import Hero from './components/Hero';
 // import Services from './components/Services';
-import Products from './components/Products';
+// import Products from './components/Products';
 // import Team from './components/Team';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 
 import { Element } from 'react-scroll';
 import { ParallaxProvider } from 'react-scroll-parallax';
@@ -19,15 +19,15 @@ function App() {
       <Element name="hero">
         <Hero />
       </Element>
-      <Element name="products">
-        {/* <Products /> */}
+      {/* <Element name="products">
+        <Products />
       </Element>
       <Element name="about">
-        {/* <About /> */}
+        <About />
       </Element>
       <Element name="contact">
-        {/* <Contact /> */}
-      </Element>
+        <Contact />
+      </Element> */}
       <Footer />
     </>
     </ParallaxProvider>
